640493fceb7bb3d319c5e556571b91adcbc64d16
Ditzzy <mukhamadnurshodiqarridho_1701617092@mhs.unj.ac.id>
2023-02-24 05:34:59 +0700

    Update instaladorenglish.sh

--- instaladorenglish.sh
+++ instaladorenglish.sh
@@ -21,7 +21,7 @@ instalartema(){
     mv pterodactylthemes.css /var/www/pterodactyl/resources/scripts/pterodactylthemes.css
     cd /var/www/pterodactyl
 
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     apt update
     apt install -y nodejs
 
